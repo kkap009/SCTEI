cbf8326407ad246cc18047a4f4c3dc2ea9a4e3f0
kkap009 <kkap009@naver.com>
2019-11-10 05:19:54 +0900

    Update

--- Web Design/MyFiles/01) JUST 쇼핑몰/index.js
+++ Web Design/MyFiles/01) JUST 쇼핑몰/index.js
@@ -1,70 +1,83 @@
 /*<nav>*/
 (function () {
-    const mainMenu = document.querySelectorAll("ul[data-main-menu]");
-    const mainMenuA = document.querySelectorAll("ul[data-main-menu] > li > a");
-    const subMenu = document.querySelectorAll("ul[data-sub-menu] > li:last-child > a");
+    const mainMenu = "ul[data-main-menu]";
+    const mainMenuLi = `${mainMenu} > li`;
+    const mainMenuQ = document.querySelectorAll(`${mainMenu}`);
+    const mainMenuA = document.querySelectorAll(`${mainMenuLi} > a`);
+    const subMenu = "ul[data-sub-menu]";
+    const subMenuLi = `${subMenu} > li`;
+    const subMenuLastA = document.querySelector(` ${mainMenu}:last-child li > ${subMenu} li:last-child > a`);
+    let status = null;
     let i = null;
 
-const maxHeightRest = function () {
-    const dataSubMenu = document.querySelectorAll("ul[data-sub-menu] > li");
+    const maxHeightRest = function () {
+        const li = document.querySelectorAll(`${subMenu} > li`);
 
-    for (i = 0; i < dataSubMenu.length; ++i) {
-        dataSubMenu[i].style.maxHeight = null;
-    }
-}
-
-const maxHeightValue = function (dataSubMenu) {
-    for (i = 0; i < dataSubMenu.length; ++i) {
-        dataSubMenu[i].style.maxHeight = `${dataSubMenu[i].scrollHeight}px`;
-    }
-}
-
-for (i = 0; i < mainMenu.length; ++i) {
-    mainMenu[i].addEventListener("mouseover", function (e) {
-        e.stopPropagation();
-        const dataSubMenuLi = e.target.querySelectorAll("ul[data-sub-menu] > li");
-
-        if (dataSubMenuLi.length) {
-            maxHeightRest();
-            maxHeightValue(dataSubMenuLi);
+        for (i = 0; i < li.length; ++i) {
+            li[i].style.maxHeight = null;
         }
-        return 0;
-    }, false);
+    }
 
-    mainMenuA[i].addEventListener("click", function (e) {
-        e.preventDefault();
-    }, false);
-
-    mainMenuA[i].addEventListener("focus", function (e) {
-        e.stopPropagation();
-        const dataSubMenuLi = e.target.parentElement.querySelectorAll("ul[data-sub-menu] > li");
-
-        if (dataSubMenuLi.length) {
-            maxHeightRest();
-            maxHeightValue(dataSubMenuLi);
+    const maxHeightValue = function (li) {
+        for (i = 0; i < li.length; ++i) {
+            li[i].style.maxHeight = `${li[i].scrollHeight}px`;
         }
-        return 0;
-    }, false)
-}
+    }
 
-for (i = 0; i < subMenu.length; ++i) {
-    subMenu[i].addEventListener("blur", function (e) {
+    const mainMenuHeight = function (e, type) {
+        e.stopPropagation();
+
+        switch (type) {
+            case "mouseover":
+                const mouseoverMenuLi = e.target.querySelectorAll(`${subMenuLi}`);
+                if (mouseoverMenuLi.length) {
+                    maxHeightRest();
+                    maxHeightValue(mouseoverMenuLi);
+                } else {
+                    return 0;
+                }
+                break;
+            case "focus":
+                const focusMenuLi = e.target.parentElement.querySelectorAll(`${subMenuLi}`);
+                if (focusMenuLi.length) {
+                    maxHeightRest();
+                    maxHeightValue(focusMenuLi);
+                } else {
+                    return 0;
+                }
+                break;
+        }
+    }
+
+    for (i = 0; i < mainMenuQ.length; ++i) {
+        mainMenuQ[i].addEventListener("mouseover", function (e) {
+            e.stopPropagation();
+            mainMenuHeight(e, e.type);
+        }, false);
+
+        mainMenuA[i].addEventListener("click", function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+        }, false);
+
+        mainMenuA[i].addEventListener("focus", function (e) {
+            e.stopPropagation();
+            mainMenuHeight(e, e.type);
+        }, false)
+    }
+
+    subMenuLastA.addEventListener("blur", function (e) {
+        e.stopPropagation();
         maxHeightRest();
-    },true);
-}
+    }, false);
 
-addEventListener("mouseover", function (e) {
-    maxHeightRest();
-}, false);
+
+    addEventListener("mouseover", function (e) {
+        maxHeightRest();
+    }, false);
 
 
 })();
 
 
 /* </nav>*/
-    
-    
-
-
-
-
