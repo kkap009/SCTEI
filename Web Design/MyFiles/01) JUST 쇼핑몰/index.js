0bff1a61a8f5c968ffc06d95172d9d5fefcef234
kkap009 <kkap009@naver.com>
2019-11-09 15:58:25 +0900

    Update

--- Web Design/MyFiles/01) JUST 쇼핑몰/index.js
+++ Web Design/MyFiles/01) JUST 쇼핑몰/index.js
@@ -1,63 +1,42 @@
-(function (){
+/*<nav>*/
+(function () {
     const mainMenu = document.querySelectorAll("ul[data-main-menu]");
     const html = document.getElementsByTagName("html");
+    let dataSubMenu = null;
     let i = null;
 
-const maxHeightRest = function(){
-    const childs = document.querySelectorAll("ul[data-sub-menu] > li");
+    const maxHeightRest = function () {
+        const dataSubMenu = document.querySelectorAll("ul[data-sub-menu] > li");
+
+        for (i = 0; i < dataSubMenu.length; ++i) {
+            dataSubMenu[i].style.maxHeight = null;
+        }
+    }
+
+    const maxHeightValue = function (dataSubMenu) {
+        for (i = 0; i < dataSubMenu.length; ++i) {
+            dataSubMenu[i].style.maxHeight = `${dataSubMenu[i].scrollHeight}px`;
+        }
+    }
+
+    for (i = 0; i < mainMenu.length; ++i) {
+        mainMenu[i].addEventListener("mouseover", function (e) {
+            e.stopPropagation();
+            const dataSubMenuLi = e.target.querySelectorAll("ul[data-sub-menu] > li");
             
-    for(i=0; i<childs.length; ++i){
-        childs[i].style.maxHeight = null;
-    }  
-}
-
-const maxHeightValue = function(dataSubMenuLi){
-    for(i=0; i<dataSubMenuLi.length; ++i){
-            dataSubMenuLi[i].style.maxHeight = `${dataSubMenuLi[i].scrollHeight}px`;
-        }   
-}
-
-for(i=0; i<mainMenu.length; ++i){
-    mainMenu[i].addEventListener("mouseover",function(e)
-    {   
-        e.stopPropagation();
-        const target = e.target;
-        const targetName = e.target.tagName.toLowerCase();
-        const targetParentName = target.parentElement.tagName.toLowerCase();
-        let children , targetChildName , dataMainMenu , dataMainMenuLi , dataSubMenuLi;
-        
-        try {
-            targetChildName = target.children[0].tagName.toLowerCase();    
-        }catch(e){
-            targetChildName = null;
-        }
-        
-        dataMainMenu = targetName.match("ul") && targetParentName.match("nav");
-        dataMainMenuLi = targetName.match("li") && targetChildName.match("ul");
-        
-        if(dataMainMenu === undefined){
-            dataMainMenu = null;    
-        }else if(dataMainMenuLi === undefined){
-            dataMainMenuLi = null;
-        }
-        
-        if(dataMainMenu){  
-            dataSubMenuLi = target.children[0].children[0].children;
-        }else if(dataMainMenuLi){
-            dataSubMenuLi = target.children[0].children;
-        }else {
+            if (dataSubMenuLi.length) {
+                maxHeightRest();
+                maxHeightValue(dataSubMenuLi);
+            }
             return 0;
-        }
-        
-        maxHeightRest();
-        maxHeightValue(dataSubMenuLi); 
-    },true);
-}
+        }, false);
+    }
 
-html[0].addEventListener("mouseover",function(e){
+    html[0].addEventListener("mouseover", function (e) {
         maxHeightRest();
-},false);
+    }, false);
 })();
+/* </nav>*/
     
     
 
