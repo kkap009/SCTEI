2f30883d0e9e99603c17b9fe3ef57a210ca3dd55
kkap009 <kkap009@naver.com>
2019-12-12 21:34:25 +0900

    Update

--- Web Design/MyFiles/ImageSlider2/imageSlider2.js
+++ Web Design/MyFiles/ImageSlider2/imageSlider2.js
@@ -1,6 +1,7 @@
 const slideTime = 3000;
 const imageDelayTime = (slideTime/100)*90;
 const className = "slideAnimation";
+const dotClassName = "active";
 const setTime = function(fun,time){
   return setTimeout(fun,time);
 };
@@ -13,12 +14,12 @@ const dot = (function(){
   function changeBy(val){
     privateCount += val;
   }
-  function reset(){
-    privateCount = 0;
+  function reset(val){
+    privateCount = val;
   }
   return {
-    valueReset : function(){
-      reset();
+    valueReset : function(val){
+      reset(val);
     },
     increment : function(){
       changeBy(1);
@@ -50,7 +51,7 @@ const addClass = function() {
     li[length].classList.add(className);
   }
 
-  button.firstElementChild.classList.add("ative");
+  button.firstElementChild.classList.add(dotClassName);
 }
 
 const next = function () {
@@ -58,12 +59,12 @@ const next = function () {
   let size = null;
   const li = document.querySelectorAll("li[data-mySlides]");
 
+  dotAtive();
+
   for(length=0; length < li.length; ++length){
     size = li[length].style.left.replace(/%$/g,'');
     li[length].style.left = (Number(size) - 100)+"%";
   }
-
-  
 
   setTime(imageDelay,imageDelayTime);
 }
@@ -76,12 +77,16 @@ function imageDelay(){
   ul.appendChild(firstChild);
 }
 
-function dotAtive(val){
-  
+function dotAtive(str){
+  const dots = document.querySelectorAll("button[data-dot]");
+  let length = null;
+
+  for(length=0; length < dots.length; ++length){
+    dots[length].classList.remove(dotClassName);
+  }
+
+  dots[dot.value()].classList.add(dotClassName);
 }
-
- 
-
 
 reset();
 addClass();
