dc8f2c550e2371120895c6c7decf9726cba8b1dd
kkap009 <kkap009@naver.com>
2019-12-17 21:33:45 +0900

    Update

---
+++ Web Design/MyFiles/ImageSlider3/ImageSlider3.js
@@ -0,0 +1,30 @@
+const carouselSlide = document.querySelector(".imageContainer");
+const carouselImages = carouselSlide.getElementsByTagName("li");
+
+// Buttons
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+
+// Counter
+let counter = 0;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.classList.add("transitionImg");
+
+nextBtn.addEventListener('click',function(){
+    ++counter;
+    carouselSlide.style.transform = "translateX("+(-size * counter) + "px)";
+});
+
+prevBtn.addEventListener('click',function(){
+    --counter;
+    carouselSlide.style.transform = "translateX("+(-size * counter) + "px)";
+});
+
+carouselSlide.addEventListener("transitionend", function(){
+   if(counter === -1){
+       carouselSlide.classList.remove("transitionImg");
+       counter = carouselImages.length - 1;
+       carouselSlide.style.transform = "translateX("+(-size * counter) + "px)";
+   }
+});
