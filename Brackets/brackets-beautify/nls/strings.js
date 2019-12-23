69d57a175e594e6a1b7827e883be4faeef16ea15
kkap009 <kkap009@naver.com>
2019-12-23 21:06:19 +0900

    Update

---
+++ Brackets/brackets-beautify/nls/strings.js
@@ -0,0 +1,17 @@
+define(function (require, exports, module) {
+    'use strict';
+
+    module.exports = {
+        root: true,
+        de: true,
+        es: true,
+        fa: true,
+        it: true,
+        ja: true,
+        pl: true,
+        'pt-br': true,
+        ro: true,
+        'zh-cn': true,
+        ru: true
+    };
+});
