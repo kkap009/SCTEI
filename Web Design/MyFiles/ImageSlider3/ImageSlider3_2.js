fc2265ec811a30070ee2a42f7a2a4a549c849eea
kkap009 <kkap009@naver.com>
2019-12-26 21:34:30 +0900

    modified:   Web Design/MyFiles/ImageSlider3/ImageSlider3.html 	modified:   Web Design/MyFiles/ImageSlider3/ImageSlider3_2.css 	modified:   Web Design/MyFiles/ImageSlider3/ImageSlider3_2.js

--- Web Design/MyFiles/ImageSlider3/ImageSlider3_2.js
+++ Web Design/MyFiles/ImageSlider3/ImageSlider3_2.js
@@ -1,44 +1,55 @@
 /*eslint-env es6*/
+/*eslint-env browser*/
 
 const carouselSlide2 = document.querySelector(".imageContainer2");
 const carouselImage2 = carouselSlide2.getElementsByTagName("li");
-
 const nextBtn2 = document.querySelector("#nextBtn2");
 const prevBtn2 = document.querySelector("#prevBtn2");
 
 let counter2 = 0;
-const size2 = carouselImage2[0].clientWidth;
-carouselSlide2.classList.add("transitionImg2");
 
-nextBtn2.addEventListener("click", function () {
+next = function (e) {
+    e.stopImmediatePropagation();
+    const size2 = carouselImage2[0].clientWidth;
     ++counter2;
-    carouselSlide2.style.left = (-size * counter2) + "px";
-});
+    if (counter2 > carouselImage2.length - 1) {
+        counter2 = 0;
+    }
+    carouselSlide2.style.left = (-size2 * counter2) + "px";
+    event = e;
+};
 
-prevBtn2.addEventListener("click", function () {
+prev = function (e) {
+    e.stopImmediatePropagation();
+    const size2 = carouselImage2[0].clientWidth;
     --counter2;
-    carouselSlide2.style.left = (-size * counter2) + "px";
-});
+    if (counter2 < 0) {
+        counter2 = carouselImage2.length - 1;
+    }
+    carouselSlide2.style.left = (-size2 * counter2) + "px";
+    event = e;
+};
 
 carouselSlide2.addEventListener("transitionend", function () {
-    let counter = null;
-    const array = new Array(carouselImage2.length -1);
-    
-    if(counter2 === carouselImage2.length-1) { /*끝 이미지*/
-        carouselSlide2.classList.remove("transitionImg2");
-        counter = 0;
-        
-        for(;counter < carouselImage2.length-1; ++counter){
-            array[counter] = carouselSlide2.removeChild(carouselImage2[counter]);
-        }
-        
+    carouselSlide2.classList.remove("transitionImg2");
+    const cloneCarouselSlide = document.querySelector(".imageContainer2");
+    const cloneCarouselImage = cloneCarouselSlide.getElementsByTagName("li");
+
+    switch (event.type) {
+        case "next":
+            break;
+        case "prev":
+            break;
+        default:
+            break;
     }
-    else if(counter2 === 0) { /*첫 이미지*/
-        carouselSlide2.classList.remove("transitionImg2");
-        
-        counter = carouselImage2.length-1;
-        
-    }
+    carouselSlide2.classList.add("transitionImg2");
 });
 
+carouselSlide2.classList.add("transitionImg2");
+nextBtn2.addEventListener("click", next);
+prevBtn2.addEventListener("click", prev);
 
+for (let i = 0; i < carouselImage2.length; ++i) {
+    carouselImage2[i].setAttribute("data-value", i + 1);
+}
